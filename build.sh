ae2cc5c24f88836db85f30f4d1394d384d6d5959
zhaoli <zhaoli.go@gmail.com>
2025-12-15 20:56:41 +0400

    feat: build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd "$(dirname "$0")/../../.." || exit 1
+
+colcon build --symlink-install \
+  --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  --packages-select lio_sam
