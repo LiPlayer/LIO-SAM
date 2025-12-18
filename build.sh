f44de07e7d100980b9bf63ab6d424d11de1fc2fc
ZhaoLi <zhaoli.go@gmail.com>
2025-12-18 17:03:24 +0400

    fix: Correct odometry and GPS topic paths in params.yaml; update build.sh directory navigation

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd "$(dirname "$0")/../../.." || exit 1
+cd "$(dirname "$0")/../.." || exit 1
 
 colcon build --symlink-install \
   --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
